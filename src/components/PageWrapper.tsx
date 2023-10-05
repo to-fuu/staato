d112f7691aa7bf99905d1961dfe28d3ac1c7d9c3
to-fuu <chebbi.ala@outlook.com>
2023-10-05 11:25:06 +0100

    handle completion of page transition globally

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,5 +1,5 @@
 'use client'
-import {ReactNode, useEffect} from "react";
+import {ReactNode, useEffect, useRef} from "react";
 import {ReactLenis} from '@studio-freight/react-lenis'
 import Cursor from "@/components/cursor";
 import {Scrollbar} from "@/components/scrollbar";
@@ -8,13 +8,17 @@ import {useMounted} from "@/hooks/useMounted";
 import gsap from "gsap"
 import ScrollTrigger from 'gsap/dist/ScrollTrigger'
 import {initParallax} from "@/lib/gsap-utils";
-import {usePathname} from "next/navigation";
+import {useParams, usePathname} from "next/navigation";
+import {Root} from "postcss";
+import Router from "@/components/Router";
+import {FinishPageTransition} from "@/components/TransitionLink";
 
 
 export default function PageWrapper({children}: { children?: ReactNode }) {
 
     const mounted = useMounted()
     const pathname = usePathname()
+
     useEffect(() => {
         if (mounted) return
 
@@ -29,22 +33,27 @@ export default function PageWrapper({children}: { children?: ReactNode }) {
             y: 120
         }, {
             opacity: 1, y: 0,
-            ease:'power1.out'
+            ease: 'power1.out'
         })
         initParallax()
+
+        FinishPageTransition()
 
     }, [pathname]);
 
     return <ReactLenis root>
-        {children}
-        <Scrollbar/>
-        <Cursor/>
-        <ScrollProgress/>
+        <Router>
+            {children}
+            <Scrollbar/>
+            <Cursor/>
+            <ScrollProgress/>
 
 
-        <div className="transition-border fixed inset-0 pointer-events-none border-stone-950"></div>
-        <div className="transition-new-page fixed inset-x-0 h-screen top-full bg-rose-500 pointer-events-none "></div>
+            <div className="transition-border fixed inset-0 pointer-events-none border-stone-950"></div>
+            <div
+                className="transition-new-page fixed inset-x-0 h-screen top-0 bg-rose-500 pointer-events-none "></div>
 
+        </Router>
 
     </ReactLenis>
 
