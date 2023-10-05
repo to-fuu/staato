97ba2b5a858661e180b035331efa0571e905136f
to-fuu <chebbi.ala@outlook.com>
2023-10-05 11:25:28 +0100

    Implement scroll offset in hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 'use client'
-import {useEffect} from "react";
-import {FinishPageTransition} from "@/components/TransitionLink";
 import Parallax from "@/components/Parallax/Parallax";
 
 export default function Home() {
 
-    useEffect(() => {
-        FinishPageTransition()
-    }, []);
 
     return (
         <div
@@ -15,13 +10,16 @@ export default function Home() {
 
             <h1 className={'text-center text-[12vw] leading-[1.2] font-bold font-heading text-rose-500 text-stroke]'}>
                 <div className={'flex  justify-center items-center'}>
-                   ST<Parallax speed={0.3}>A</Parallax><Parallax speed={0.4}>A</Parallax><Parallax speed={0.5}>A</Parallax>TO
+                    ST<Parallax scrollStartOffset={120} speed={0.3}>A</Parallax><Parallax scrollStartOffset={120}
+                                                                                          speed={0.6}>A</Parallax><Parallax
+                    scrollStartOffset={120} speed={1.2}>A</Parallax>TO
                 </div>
 
                 <div className={'flex justify-center items-center'}>
-                    <Parallax speed={0.4} className={'inline-block'}>T</Parallax>E<Parallax
-                    speed={0.3}>MP</Parallax>LA<Parallax
-                    speed={0.5}>TE</Parallax>
+                    <Parallax scrollStartOffset={120} speed={0.4} className={'inline-block'}>T</Parallax>E<Parallax
+                    scrollStartOffset={120} speed={0.3}>MP</Parallax>LA<Parallax
+                    scrollStartOffset={120} speed={0.5}>T</Parallax><Parallax
+                    scrollStartOffset={120} speed={0.7}>E</Parallax>
                 </div>
 
             </h1>
@@ -33,7 +31,7 @@ export default function Home() {
                 )}
             </p>
 
-            <p className={'mt-auto text-[12vw]  font-heading text-center'}>
+            <p id={'foot'} className={'mt-auto text-[12vw]  font-heading text-center'}>
                 <Parallax speed={-0.5}>H</Parallax>A<Parallax>PP</Parallax>Y <br/>{' '}<Parallax
                 speed={-0.5}>CO</Parallax><Parallax speed={0.4}>DING</Parallax> <Parallax speed={-0.4}>:)</Parallax>
             </p>
