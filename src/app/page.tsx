ba7030c4dea873790f024e83e38846bc673633d1
to-fuu <chebbi.ala@outlook.com>
2023-10-05 21:22:57 +0100

    update hero title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,20 @@ export default function Home() {
             className={'min-h-[200vh] py-24 flex flex-col items-center px-8 relative'}>
 
             <h1 className={'text-center text-[12vw] leading-[1.2] font-bold font-heading text-rose-500 text-stroke]'}>
+
+                <div className={'flex  justify-center items-center'}>
+                    <Parallax scrollStartOffset={120} speed={0.3}>3</Parallax>.<Parallax scrollStartOffset={120}
+                                                                                          speed={0.6}>2</Parallax>.<Parallax
+                    scrollStartOffset={120} speed={1.2}>1</Parallax>
+                </div>
+
                 <div className={'flex  justify-center items-center'}>
                     ST<Parallax scrollStartOffset={120} speed={0.3}>A</Parallax><Parallax scrollStartOffset={120}
                                                                                           speed={0.6}>A</Parallax><Parallax
                     scrollStartOffset={120} speed={1.2}>A</Parallax>TO
                 </div>
 
-                <div className={'flex justify-center items-center'}>
-                    <Parallax scrollStartOffset={120} speed={0.4} className={'inline-block'}>T</Parallax>E<Parallax
-                    scrollStartOffset={120} speed={0.3}>MP</Parallax>LA<Parallax
-                    scrollStartOffset={120} speed={0.5}>T</Parallax><Parallax
-                    scrollStartOffset={120} speed={0.7}>E</Parallax>
-                </div>
+
 
             </h1>
             <p className={'text-4xl font-semibold uppercase mt-24 '}>
